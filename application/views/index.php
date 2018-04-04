eb1d587b6e8f5ad2670e6cb30ea6a31d8b26f7cb
RifqiSambas <rifqisambas@outlook.com>
2018-04-05 05:05:23 +0700

    1

--- application/views/index.php
+++ application/views/index.php
@@ -23,8 +23,33 @@
 </head>
 
 <body>
+	<div class="mdl-layout mdl-js-layout mdl-color--grey-100">
+		<main class="mdl-layout__content">
+			<div class="mdl-card mdl-shadow--6dp">
+				<div class="mdl-card__title mdl-color--primary mdl-color-text--white">
+					<h2 class="mdl-card__title-text">SISTIK</h2>
+				</div>
+				<div class="mdl-card__supporting-text">
+					<form action="<?php echo base_url();?>index.php/c_login/auth" method="post">
+						<div class="mdl-textfield mdl-js-textfield">
+							<input class="mdl-textfield__input" type="text" id="username" />
+							<label class="mdl-textfield__label" for="username">Username</label>
+						</div>
+						<div class="mdl-textfield mdl-js-textfield">
+							<input class="mdl-textfield__input" type="password" id="userpass" />
+							<label class="mdl-textfield__label" for="userpass">Password</label>
+						</div>
+					</form>
+				</div>
+				<div class="mdl-card__actions mdl-card--border">
+					<button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Log in</button>
+				</div>
+			</div>
+		</main>
+	</div>
 	<main>
-		<div>
+	
+		<!-- <div>
 			<form action="<?php echo base_url();?>index.php/c_login/create" method="post">
 				<div>
 					<input type="text" name="username" placeholder="username">
@@ -37,16 +62,7 @@
 				</div>
 			</form>
 		</div>
-		<div>
-			<form action="<?php echo base_url();?>index.php/c_login/auth" method="post">
-				<div>
-					<input type="text" name="username" placeholder="username">
-					<input type="password" name="password" placeholder="password">
-					<input type="submit" name="submit" value="Sign In">
-				</div>
-			</form>
-		</div>
-	</main>
+	</main> -->
 </body>
 
 </html>
